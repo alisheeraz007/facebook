f9807ca5add512d4fcc30890b08cd6aa3bfc1138
alisheeraz007 <alisheeraz007@gmail.com>
2019-06-24 17:51:49 +0500

    Updates

---
+++ precache-manifest.2b656f993359d515a32336b1563cfc6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cece245b76e04ec4221ec206dc55cc63",
+    "url": "/Facebook/index.html"
+  },
+  {
+    "revision": "2e5e2a9d86bd7a5996c3",
+    "url": "/Facebook/static/css/main.c497f69c.chunk.css"
+  },
+  {
+    "revision": "c1e32f2cf1d43351194f",
+    "url": "/Facebook/static/js/2.21174cd3.chunk.js"
+  },
+  {
+    "revision": "2e5e2a9d86bd7a5996c3",
+    "url": "/Facebook/static/js/main.7d7a410d.chunk.js"
+  },
+  {
+    "revision": "683aacf022953b872dd9",
+    "url": "/Facebook/static/js/runtime~main.8a2e1ff3.js"
+  },
+  {
+    "revision": "97097f004c79a61fecefea45578f7dde",
+    "url": "/Facebook/static/media/facebook-style-homepage.97097f00.jfif"
+  }
+]);
